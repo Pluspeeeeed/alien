4f0f1e39b7a6aa814b99d4a34483d71bb0ccf32b
txrTanzi <txrtanzi@gmail.com>
2021-05-25 14:23:56 +0800

    Add ship.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -12,6 +13,7 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
 
     def run_game(self):
         """main loop"""
@@ -21,8 +23,9 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            '''draw bg color'''
+            '''draw screen in loop'''
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             '''make drawn screen visible'''
             pygame.display.flip()
